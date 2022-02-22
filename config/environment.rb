88a970e69a8885b1bfaf6539efb1e0f45a31529f
mborreros <mlbsindia@gmail.com>
2022-02-22 15:21:45 -0500

    commit tree ab1e8580c4d16cd6fcfcb1fec3adc7ebe00c903a

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,11 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
-require_relative "../app/models/artist"
+
+# ActiveRecord::Base.establish_connection(
+#   adapter: "sqlite3",
+#   database: "db/artists.sqlite"
+# )
+
+require_relative "../app/models/artist.rb"
+
